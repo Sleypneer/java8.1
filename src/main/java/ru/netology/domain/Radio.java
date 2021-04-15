bc30be8d7f86960e87c8be92bdfff3d25f8a2c9f
sleypneer <sleypneer@mail.ru>
2021-04-15 15:29:00 +0300

    add project

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,127 @@
+package ru.netology.domain;
+
+public class Radio {
+    private int currentStation;
+    private int maxStation;
+    private int minStation;
+    private int currentVolume;
+    private int maxVolume;
+    private int minVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > maxStation) {
+            setCurrentStation(getMinStation());
+            return;
+        }
+        if (currentStation < minStation) {
+            setCurrentStation(getMaxStation());
+            return;
+        }
+
+        this.currentStation = currentStation;
+    }
+
+    public int getMaxStation() {
+        maxStation = 9;
+        return maxStation;
+    }
+
+    public void setMaxStation(int maxStation) {
+        this.maxStation = maxStation;
+    }
+
+    public int getMinStation() {
+        minStation = 0;
+        return minStation;
+    }
+
+    public void setMinStation(int minStation) {
+        this.minStation = minStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            return;
+        }
+        if (currentVolume < minVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public int getMaxVolume() {
+        maxVolume = 10;
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getMinVolume() {
+        minVolume = 0;
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+    public void increaseCurrentStation() {
+        int increaseStation = getCurrentStation() + 1;
+        setCurrentStation(increaseStation);
+    }
+    public void decreaseCurrentStation() {
+        int decreaseStation = getCurrentStation() - 1;
+        setCurrentStation(decreaseStation);
+    }
+
+    public void increaseCurrentVolume() {
+        int increaseVolume = getCurrentVolume() + 1;
+        setCurrentVolume(increaseVolume);
+    }
+    public void decreaseCurrentVolume() {
+        int decreaseVolume = getCurrentVolume() - 1;
+        setCurrentVolume(decreaseVolume);
+    }
+
+    public void firstStation () {
+        setCurrentStation(0);
+    }
+    public void secondStation () {
+        setCurrentStation(1);
+    }
+    public void thirdStation () {
+        setCurrentStation(2);
+    }
+    public void fourthStation () {
+        setCurrentStation(3);
+    }
+    public void fifthStation () {
+        setCurrentStation(4);
+    }
+    public void sixthStation () {
+        setCurrentStation(5);
+    }
+    public void seventhStation () {
+        setCurrentStation(6);
+    }
+    public void eighthStation () {
+        setCurrentStation(7);
+    }
+    public void ninthStation () {
+        setCurrentStation(8);
+    }
+    public void tenthStation () {
+        setCurrentStation(9);
+    }
+
+}
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,119 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RadioTest {
+
+    @Test
+    void increaseCurrentStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(radio.getMaxStation());
+        radio.increaseCurrentStation();
+        assertEquals(radio.getMinStation(), radio.getCurrentStation());
+    }
+
+    @Test
+    void decreaseCurrentStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(radio.getMinStation());
+        radio.decreaseCurrentStation();
+        assertEquals(radio.getMaxStation(), radio.getCurrentStation());
+    }
+
+    @Test
+    void increaseCurrentVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(radio.getMaxVolume());
+        radio.increaseCurrentVolume();
+        assertEquals(radio.getMaxVolume(), radio.getCurrentVolume());
+    }
+
+    @Test
+    void decreaseCurrentVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(radio.getMinVolume());
+        radio.decreaseCurrentVolume();
+        assertEquals(radio.getMinVolume(), radio.getCurrentVolume());
+    }
+
+    @Test
+    void firstStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(radio.getMaxStation());
+        radio.firstStation();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void secondStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(radio.getMaxStation());
+        radio.secondStation();
+        assertEquals(1, radio.getCurrentStation());
+    }
+
+    @Test
+    void thirdStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(radio.getMaxStation());
+        radio.thirdStation();
+        assertEquals(2, radio.getCurrentStation());
+    }
+
+    @Test
+    void fourthStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(radio.getMaxStation());
+        radio.fourthStation();
+        assertEquals(3, radio.getCurrentStation());
+    }
+
+    @Test
+    void fifthStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(radio.getMaxStation());
+        radio.fifthStation();
+        assertEquals(4, radio.getCurrentStation());
+    }
+
+    @Test
+    void sixthStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(radio.getMaxStation());
+        radio.sixthStation();
+        assertEquals(5, radio.getCurrentStation());
+    }
+
+    @Test
+    void seventhStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(radio.getMaxStation());
+        radio.seventhStation();
+        assertEquals(6, radio.getCurrentStation());
+    }
+
+    @Test
+    void eighthStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(radio.getMaxStation());
+        radio.eighthStation();
+        assertEquals(7, radio.getCurrentStation());
+    }
+
+    @Test
+    void ninthStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(radio.getMaxStation());
+        radio.ninthStation();
+        assertEquals(8, radio.getCurrentStation());
+    }
+
+    @Test
+    void tenthStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(radio.getMaxStation());
+        radio.tenthStation();
+        assertEquals(9, radio.getCurrentStation());
+    }
+}
